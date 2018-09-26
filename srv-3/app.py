b0b4fb87eb9855c89cbd488323cdf14531daf186
Daniel Requena <requena@gmail.com>
2018-09-26 00:51:07 -0300

    srv3 now connects to DB

--- srv-3/app.py
+++ srv-3/app.py
@@ -1,6 +1,17 @@
 from flask import Flask, jsonify
 import os
 import json
+import mysql.connector
+
+databasehost = os.getenv("DATABASEHOST","localhost")
+databaseuser = os.getenv("DATABASEUSER","root")
+databasepass = os.getenv("DATABASEPASS","123456")
+databasename = os.getenv("DATABASENAME","data")
+databaseport = os.getenv("DATABASEPORT","3306")
+
+nextServiceHost = os.getenv("NEXT_SERVICE","localhost")
+nextServicePort = os.getenv("NEXT_SERVICE_PORT","5003")
+nextService = "http://"+nextServiceHost+":"+nextServicePort
 
 app = Flask(__name__)
 
@@ -8,4 +19,21 @@ app_data = {'version': '1.2', 'name': 'service3', 'hostname': os.uname()[1], "st
 
 @app.route('/')
 def default_root():
-    return jsonify({app_data["name"]:app_data})
+    response = { app_data["name"]: app_data }
+
+    try:
+        mydb = mysql.connector.connect(host=databasehost,port=databaseport,user=databaseuser,passwd=databasepass,database=databasename)
+        mycursor = mydb.cursor()
+        mycursor.execute("SELECT * FROM tv_show ORDER BY RAND() LIMIT 1;")
+        myresult = mycursor.fetchone()
+        mycursor.close()
+        mydb.close()
+
+        result={"db_data":{"id":myresult[0],"name":myresult[1],"score":myresult[2],"year":myresult[3]}}
+
+        app_data.update(result.items())
+    except:
+        app_data.update({"db_data":"Failed to connect to DB"})
+        app.logger.error("Failed to connect to DB")
+    
+    return jsonify(response)

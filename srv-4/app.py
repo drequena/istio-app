f8e139f53a943465d2814a4f4f8c0cb1be58198b
Daniel Requena <requena@gmail.com>
2018-11-13 18:17:04 -0200

    add rand request and better return

--- srv-4/app.py
+++ srv-4/app.py
@@ -2,31 +2,36 @@ from flask import Flask, jsonify
 import os
 import json
 import requests
+import random
 
-nextServiceHost = os.getenv("NEXT_SERVICE","jsonplaceholder.typicode.com")
+nextServiceHost = os.getenv("NEXT_SERVICE","jsonplaceholderr.typicode.com")
 nextServicePort = os.getenv("NEXT_SERVICE_PORT","443")
-nextServiceRoute = os.getenv("NEXT_SERVICE_ROUTE","/users/1")
-nextService = "https://"+nextServiceHost+":"+nextServicePort+nextServiceRoute
+nextServiceRoute = os.getenv("NEXT_SERVICE_ROUTE","/users/")
 app = Flask(__name__)
 
-app_data = {'version': '2.1', 'name': 'service4', 'hostname': os.uname()[1], "status":"up"}
+app_data = {'version': '2.1', 'name': 'service4', 'hostname': os.uname()[1], "status":"up" }
 
 @app.route('/')
 def default_root():
-    response = { app_data["name"]: app_data }
+    rand_user = random.randint(1,10)
+    nextService = "https://"+nextServiceHost+":"+nextServicePort+nextServiceRoute
+    response = {app_data["name"]: app_data}
     
     returnedJson = {}
 
     try:
-        r = requests.get(nextService)
+        r = requests.get(nextService+str(rand_user))
         returnedJson = r.json()
+        app_data.update({"API_Data":returnedJson})
+
     except requests.exceptions.ConnectionError:
         app.logger.error("Error while connecting to %s", nextService)
         returnedJson = {nextService:{"status":"down"}}
+        app_data.update(returnedJson)
     except json.decoder.JSONDecodeError:
         app.logger.error("Json parsing error!")
         returnedJson = {nextService:{"status":"down"}}
+        app_data.update(returnedJson)
 
-    response.update(returnedJson.items())
 
     return jsonify(response)
